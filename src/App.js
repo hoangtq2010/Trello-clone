1c81d07cd044b9b878122cd3268dbf5036a2915c
hoang <36702685+hoangtq2010@users.noreply.github.com>
2021-09-11 14:48:04 +0700

    Clean Architecture

--- src/App.js
+++ src/App.js
@@ -1,108 +1,17 @@
-
+import React from 'react'
 import './App.scss'
+
+// custom components
+import AppBar from 'components/AppBar/AppBar';
+import BoardBar from 'components/BoardBar/BoardBar';
+import BoardContent from 'components/BoardContent/BoardContent';
 
 function App() {
   return (
     <div className="trello-master">
-      <nav className="navbar app">App Bar</nav>
-      <nav className="navbar board">Board Bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://petnhatrang.com/wp-content/uploads/2020/10/0_yP7NdX2wWQVilq1_-467x400.png" alt="Hoangkissdev-alt-img"/>
-              Title : HoangKissDev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://petnhatrang.com/wp-content/uploads/2020/10/0_yP7NdX2wWQVilq1_-467x400.png" alt="Hoangkissdev-alt-img"/>
-              Title : HoangKissDev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://petnhatrang.com/wp-content/uploads/2020/10/0_yP7NdX2wWQVilq1_-467x400.png" alt="Hoangkissdev-alt-img"/>
-              Title : HoangKissDev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://petnhatrang.com/wp-content/uploads/2020/10/0_yP7NdX2wWQVilq1_-467x400.png" alt="Hoangkissdev-alt-img"/>
-              Title : HoangKissDev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://petnhatrang.com/wp-content/uploads/2020/10/0_yP7NdX2wWQVilq1_-467x400.png" alt="Hoangkissdev-alt-img"/>
-              Title : HoangKissDev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://petnhatrang.com/wp-content/uploads/2020/10/0_yP7NdX2wWQVilq1_-467x400.png" alt="Hoangkissdev-alt-img"/>
-              Title : HoangKissDev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar/>
+      <BoardContent/>
     </div>
   );
 }
