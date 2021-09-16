0a847da00b9b35533b0e316bee2f2f070f2f5616
hoang <36702685+hoangtq2010@users.noreply.github.com>
2021-09-16 16:23:10 +0700

    Update Remove Column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,18 +1,79 @@
 import Card from 'components/Card/Card'
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import './Column.scss'
 
 import { mapOrder } from '../../utilities/sorts'
 
 import { Container, Draggable } from 'react-smooth-dnd'
 
+import { Dropdown, Form } from 'react-bootstrap'
+import ConfirmModal from '../../components/Common/ConfirmModal'
+import { MODAL_ACTION_CONFIRM } from 'utilities/constants'
+import { saveContentAfterPressEnter, selectAllInLineText } from '../../utilities/contentEdittable'
+
 function Column(props) {
-    const { column, onCardDrop } = props //data tu cha->con
+    const { column, onCardDrop, onUpdateColumn } = props //data tu cha->con
     const cards = mapOrder(column.cards, column.cardOrder, 'id')
+
+    const [showConfirmModal, setShowConfirmModal] = useState(false)
+    const toggleShowConfirmModal = () => setShowConfirmModal(!showConfirmModal)
+
+    const [columnTitle, setColumnTitle] = useState('')
+    const handleColumnTitleChange = useCallback((e) => setColumnTitle(e.target.value), [])
+
+    useEffect(() => {
+        setColumnTitle(column.title)
+    }, [column.title])
+
+    const onConfirmModalAction = (type) => {
+        if (type === MODAL_ACTION_CONFIRM) {
+            const newColumn = {
+                ...column,
+                _destroy: true
+            }
+            onUpdateColumn(newColumn)
+        }
+        toggleShowConfirmModal()
+    }
+
+    const handleColumnTitleBlur = () => {
+        const newColumn = {
+            ...column,
+            title: columnTitle
+        }
+        onUpdateColumn(newColumn)
+    }
 
     return (
         <div className="column">
-            <header className="column-drag-handle">{column.title}</header>
+            <header className="column-drag-handle">
+                <div className="column-title">
+                    <Form.Control
+                        size="sm"
+                        type="text"
+                        className="trello-content-editable"
+                        value={columnTitle}
+                        onChange={handleColumnTitleChange}
+                        onBlur={handleColumnTitleBlur}
+                        onKeyDown={saveContentAfterPressEnter}
+                        onClick={selectAllInLineText}
+                        onMouseDown={e => e.preventDefault()}
+                        spellCheck="false"
+                    />
+                </div>
+                <div className="column-dropdown-actions">
+                    <Dropdown>
+                        <Dropdown.Toggle id="dropdown-basic" size="sm" className="dropdown-btn" />
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item>Add card...</Dropdown.Item>
+                            <Dropdown.Item onClick={toggleShowConfirmModal}>Remove column...</Dropdown.Item>
+                            <Dropdown.Item>Move all cards in this column(beta)...</Dropdown.Item>
+                            <Dropdown.Item>Archive all cards in this column(beta)...</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+            </header>
             <div className="card-list">
                 <Container
                     orientation="vertical" //default
@@ -40,6 +101,13 @@ function Column(props) {
                     <i className="fa fa-plus icon" />Add another card
                 </div>
             </footer>
+
+            <ConfirmModal
+                show={showConfirmModal}
+                onAction={onConfirmModalAction}
+                title="Remove column"
+                content={`Are you sure you want to remove <strong>${column.title}</strong>.<br /> All related cards will also be removed `}
+            />
         </div>
     )
 }
---
+++ src/utilities/contentEdittable.js
@@ -0,0 +1,13 @@
+//onKeyDown
+export const saveContentAfterPressEnter = (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault()
+        e.target.blur()
+    }
+}
+
+//Select all input value when click
+export const selectAllInLineText = (e) => {
+    e.target.focus()
+    e.target.select()
+}
